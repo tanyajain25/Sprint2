2143561a81e57444e87cda4684dd5db664743721
tanyajain25 <jaintanyatj25@gmail.com>
2020-05-18 14:12:28 +0530

    commit

---
+++ usermngt/src/main/java/org/cap/usermngt/UserManagementApplication.java
@@ -0,0 +1,38 @@
+package org.cap.usermngt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * this is equal to three annotations 1) @Configuration 2)@ComponentScan
+ * 3)@EnableAutoConfiguration
+ */
+@SpringBootApplication
+@EnableTransactionManagement
+public class UserManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedMethod("*");
+		src.registerCorsConfiguration("/**", configuration);
+		return new CorsFilter(src);
+	}
+}
